2509149830ab16dd652be999d7d47f4999b66919
Elmira <3lmirah0sse@gmail.com>
2026-07-13 17:34:26 +0330

    Added PowerShell network report script

---
+++ scripts/network-report.ps1
@@ -0,0 +1,10 @@
+Write-Host "===== Home Network Report ====="
+Write-Host ""
+
+Write-Host "Computer Name:"
+Write-Host $env:COMPUTERNAME
+
+Write-Host ""
+
+Write-Host "Current Date:"
+Write-Host (Get-Date)
