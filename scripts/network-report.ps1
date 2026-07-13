1b6e44c0a57a2993acb0672bd7dfacce8742da69
Elmira <3lmirah0sse@gmail.com>
2026-07-13 18:13:18 +0330

    Added subnet mask and DNS information

--- scripts/network-report.ps1
+++ scripts/network-report.ps1
@@ -21,3 +21,20 @@ $env:COMPUTERNAME | Out-File $reportPath -Append
 ipconfig | Out-File $reportPath -Append
 
 Write-Host "Network report created successfully!"
+
+"" | Out-File $ReportPath -Append
+
+"Subnet Mask:" | Out-File $ReportPath -Append
+(ipconfig | Select-String "Subnet Mask") | Out-File $ReportPath -Append
+
+"" | Out-File $ReportPath -Append
+
+"" | Out-File $ReportPath -Append
+
+"DNS Servers:" | Out-File $ReportPath -Append
+
+Get-DnsClientServerAddress |
+Where-Object {$_.ServerAddresses} |
+ForEach-Object {
+    $_.ServerAddresses
+} | Out-File $ReportPath -Append
